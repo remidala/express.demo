28642246bdeec0128e4f9b4a0d151cdce994a717
Mihaly Reiter <100424934+remidala@users.noreply.github.com>
2022-09-04 19:27:41 +0100

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -3,10 +3,32 @@ const express = require("express");
 // Initialize express
 const app = express();
 const PORT = process.env.PORT || 8080;
-// parse JSON
-app.use(express.json());
-// parse URL encoded data
-app.use(express.urlencoded({ extended: true }));
+
+//app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Hi my name is Dan");
+});
+
+app.get("/hello", (req, res) => {
+    res.send({name: 'Dan'});
+});
+
+app.get("/info", (req, res) => {
+    res.send({name:"Dan", age:37})
+    console.log(req.query);
+    if (req.query.age){
+        req.send({age: req.query.age});
+    } else{
+        res.send({error: "No age found"});
+    }
+});
+
+app.get("/useres/ :id", (req, res) => {
+    console.log(req.params.id);
+    res.send({id: req.params.id});
+});
+
 // create a server
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
